c1a95c73476d28424bef325f7ffd3fdfc0b77be6
rinogrego <rino.grego@gmail.com>
2022-05-09 01:27:24 +0700

    setting SECRET_KEY

--- capstone/settings.py
+++ capstone/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qvh626q(ftp)nwiqw^kekr!3%_51%hfpqa$1w%kuncchc3s^$('
+# SECRET_KEY = 'qvh626q(ftp)nwiqw^kekr!3%_51%hfpqa$1w%kuncchc3s^$(' (already changed)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
