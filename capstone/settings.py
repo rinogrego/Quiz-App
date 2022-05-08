8cc479ecf664a357ff3f90ffd1c8c146578e0b73
rinogrego <rino.grego@gmail.com>
2022-05-09 01:07:48 +0700

    add default_auto_field

--- capstone/settings.py
+++ capstone/settings.py
@@ -128,3 +128,9 @@ MEDIA_ROOT_PROFILE = os.path.join(BASE_DIR, 'media/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
